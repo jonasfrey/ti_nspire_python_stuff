8e285b04b41f07e7b47a13b6d28e5170aae83831
Jonas Frey <y@y.com>
2023-01-08 01:07:49 +0100

    square mul

--- f_square_and_multiply.py
+++ f_square_and_multiply.py
@@ -35,20 +35,30 @@ def f_square_and_multiply(n_basis, n_exponent):
     n_exp = 0
     n_prod = 1
     n_index = 0
+    n_muls = 0
     while(n_index < len(s_exponent_as_binary)):
+        s = s_exponent_as_binary[n_index]
+        print("--------------")
+        print("bit position: ")
+        print(''.join([(s if n == n_index else "x") for n in range(0, len(s_exponent_as_binary))]))
         if(n_index > 0):
             n_prod = n_prod ** 2
             print("n_prod = n_prod ** 2")
             print("n_prod = "+str(n_prod)+" ** 2")
-        if(s_exponent_as_binary[n_index] == '1'):
+            n_muls+=1
+        if(s == '1'):
             n_prod = n_prod * n_basis
             print("n_prod = n_prod * n_basis")
-
             print("n_prod = "+str(n_prod)+" * "+ str(n_basis))
+            n_muls+=1
 
         n_index += 1
 
-    print(n_prod)
+
+    print("--------------")
+    print(str(n_basis)+"^"+str(n_exponent)+" = "+str(n_prod))
+    print("amount of multiplication steps: "+str(n_muls))
+
 
     return n_prod
 
